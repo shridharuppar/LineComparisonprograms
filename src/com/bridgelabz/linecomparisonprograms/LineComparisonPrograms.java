fde4f1da19977bbdc4d392ecbcf4b6b5957a5201
shridharuppar <upparshridhar@gmail.com>
2023-03-24 13:03:45 +0530

    added: .gitignore and README.md files are added

---
+++ src/com/bridgelabz/linecomparisonprograms/LineComparisonPrograms.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.linecomparisonprograms;
+
+public class LineComparisonPrograms {
+   public static void main(String[] args) {
+	  
+	
+	System.out.println("Welcome message to Line Comparion program");
+}
+}
